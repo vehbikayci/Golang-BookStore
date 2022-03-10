8a0e253c2ada7f219d84f22080aed7b06ab34191
vehbikayci <vehbikkayci@gmail.com>
2022-03-10 23:25:20 +0300

    First Commit

---
+++ pkg/models/book.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/vehbikayci/go-bookstore/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"";json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBook() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db.Where("Id=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("Id=?", Id).Delete(&book)
+	return book
+}
